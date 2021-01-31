e4fee4bc0f98320efe47382ba9ee8c6823eef2d0
mazurkevichandrei <mazurkevi4@gmail.com>
2021-01-31 16:45:09 +0300

    create_all_sliders_v1

--- ACTIVEBOX/js/script.js
+++ ACTIVEBOX/js/script.js
@@ -3,3 +3,19 @@ $(document).ready(function(){
         dots:true
     });
 })
+
+$(document).ready(function(){
+    $('.works__box__slider').slick({
+        dots:true,
+    });
+    $('.works__box__slider').slick('setPosition');
+})
+
+$(document).ready(function(){
+    $('.team__cards__slider').slick({
+        slidesToShow:1,
+        dots:true,
+    });
+    $('.team__cards__slider').slick('setPosition');
+})
+
